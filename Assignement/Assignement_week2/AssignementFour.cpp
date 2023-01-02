b0110c167c5c371cd0a6a83fb10679095a64e84e
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2023-01-02 01:16:39 +0100

    Create AssignementFour.cpp

---
+++ Assignement/Assignement_week2/AssignementFour.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* ===== OutPut Final ======
+|=======================|
+|== Elzero Web School ==|
+|==        C++        ==|
+|=======================|
+*/
+int main()
+{
+  // Solution One
+  cout << "|=======================|";
+  cout << "|== Elzero Web School ==|";
+  cout << "|==        C++.       ==|";
+  cout << "|=======================|";
+  // Solution Two
+  cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|";
+  return 0;
+}
