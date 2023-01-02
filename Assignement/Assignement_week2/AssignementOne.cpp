251f720a4c9555da82f882bc4ec0ada1f9522cad
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2023-01-02 01:00:33 +0100

    Update the Assignement
    Solution Of Assignement 1 & 2.

---
+++ Assignement/Assignement_week2/AssignementOne.cpp
@@ -0,0 +1,34 @@
+/*
+include <"iostream">
+
+Main()
+{
+  std::out < "Line One/n";
+  std::out < "Line Two/n";
+  std::out < "Line Three/n";
+  return 0;
+}
+/*
+///////// Solution /////////
+#include <iostream>
+
+int main()
+{
+  std::cout << "Line One\n";
+  std::cout << "Line Two\n";
+  std::cout << "Line Three\n";
+  return 0;
+}
+
+//=====| Assignement 2 Week One |=====
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- wcout
+  -- wcin
+  -- cerr
+  -- clog
+  -- wcerr
+  -- wclog
+*/
