9fb78953fa0aca3f654f36f628092f4474ab64cb
ismail9875 <ismail9875@gmail.com>
2023-01-03 12:27:35 +0100

    Add Some Assignements Files.

---
+++ Assignement/Assignement_week2/AssignementThreeWeekTwo.cpp
@@ -0,0 +1,16 @@
+/*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ NoValid ] => Cannot Start a Variable With a Nmber.
+  __name      [ Valid ] => Best Practice
+  name@name   [ NoValid ] => Cannot Include A Special Character.
+  name10name  [ Valid ] => Good Practice
+  name!name   [ NoValid ] => Cannot Include a Special Character.
+  first_NAME  [ Valid ] => Bad Practice
+  first_name  [ Valid ] => Good Practice
+  firstName   [ Valid  ] => Good Practice
+  first name  [ NoValid ] => Cannot include Spaces
+  fn          [ Valid ] => Bad Practice >> 
+  public      [ No Valid ] => Is a Keywords C++
+  Public      [ Valid ] => Good Practice
+*/
