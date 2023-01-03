1df7c24f7195d3913daecdf64f74c8cb05d3bac7
ismail9875 <ismail9875@gmail.com>
2023-01-03 11:53:42 +0100

    Modified Volumecalculator file & add Assignementfive file

--- Assignement/Assignement_week2/VolumeCalculator.cpp
+++ Assignement/Assignement_week2/VolumeCalculator.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+Elzero Web School Fundamentas Of Programming Using C++
+Application Vedeo 17
+*/
+
 int main()
 {
-    int value ;
-    cout << "|=========================================|\n";
-    cout << "|=========== Volume Calculator ===========|\n";
-    cout << "|=========================================|\n";
-    cout << "Please Enter A Value On KiloByte : \n";
-    cin >> value;
-    cout << "Your Value Is : " << value << "\n";
-    
-    //؛;؛;؛; Calculat Byte ؛;؛;؛;//
-    
-    int Byte = value * 1024;
-    cout << "Is Equal : " << Byte << " Byte\n";
-    
-    //؛;؛;؛; Calculat Byte ؛;؛;؛;//
-
-    int bit = Byte * 1024;
-    cout << "Is Equal : " << bit << " bit \n";
-    return 0;
+   int value ;
+   cout << "|=========================================|\n";
+   cout << "|=========== Volume Calculator ===========|\n";
+   cout << "|=========================================|\n";
+   cout << "Please Enter A Value On KiloByte : \n";
+   cin >> value;
+   cout << "Your Value Is : " << value << "\n";
+   //؛;؛;؛; Calculat Byte ؛;؛;؛;//
+   int Byte = value * 1024;
+   cout << "Is Equal : " << Byte << " Byte\n";
+   //؛;؛;؛; Calculat Byte ؛;؛;؛;//
+   int bit = Byte * 1024;
+   cout << "Is Equal : " << bit << " bit \n";
+   return 0;
 }
