4d4820a7bbd680992bb886a2eada19fbeed15bd7
ismail9875 <ismail9875@gmail.com>
2023-01-04 23:53:43 +0100

    Add Some Assognement

---
+++ Assignement/Assignement_week2/AssignementNineWeekTwo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+ int Num ; 
+ cout << "Please Tape a number : ";
+ cin >> Num ;
+ int Multiple = Num * Num;
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
---
+++ Assignement/Assignement_week2/AssignementSexWeekTwo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Do Not Edit Below
+  int a = 210, b = -50, c = -40;
+  int Result = a -(b+c);
+  // Write Your cout Code Here
+  cout << "Your Result is : " << Result << "\n";
+  return 0;
+}
