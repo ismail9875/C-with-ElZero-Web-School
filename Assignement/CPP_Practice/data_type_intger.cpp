59e7e14a59c91809218ba719896b2b3d00c0b211
ismail9875 <ismail9875@gmail.com>
2023-01-07 01:17:11 +0100

    Modify data type file

---
+++ Assignement/CPP_Practice/data_type_intger.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num = 10;
+   num = 10 + 0.5;
+   cout << " The Size Of Variable Num On Memory Is : " << sizeof(num) << endl; // endl replace \n  
+   cout << num;
+   return 0;
+}
