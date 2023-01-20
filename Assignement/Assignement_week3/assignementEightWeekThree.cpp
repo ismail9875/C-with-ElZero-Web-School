2f81d494a7ae42bc4eac58510f310fd27e4594b8
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2023-01-20 13:46:29 +0100

    edit file

--- Assignement/Assignement_week3/assignementEightWeekThree.cpp
+++ Assignement/Assignement_week3/assignementEightWeekThree.cpp
@@ -6,11 +6,11 @@ int main()
    // Edit Anything Except Values
    int a = 100;
    int b = 15001500;
-   double c = 100.54565746;
+   long double c = 100.54565746;
 
    // Do Not Edit
    cout << sizeof(short(a)) << " Bytes\n"; // 2 Bytes
    cout << sizeof(long(b)) << " Bytes\n"; // 8 Bytes
-  cout << sizeof((c)) << " Bytes\n"; // 16 Bytes
+   cout << sizeof((c)) << " Bytes\n"; // 16 Bytes
    return 0;
 }
